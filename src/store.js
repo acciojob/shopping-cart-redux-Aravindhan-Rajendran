55ff977d246994e42599853252aa658e188010a4
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 19:22:35 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  // Add Redux DevTools extension support if needed
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
