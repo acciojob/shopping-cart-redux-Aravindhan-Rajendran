93c4d0014af96c69dc33d511e2ce1260a0ae4355
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 19:28:34 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,35 +5,17 @@ import { removeFromCart, increaseQuantity, decreaseQuantity } from '../actions';
 const Cart = ({ cartItems }) => {
   const dispatch = useDispatch();
 
-  const handleRemove = (id) => {
-    dispatch(removeFromCart(id));
-  };
-
-  const handleIncrease = (id) => {
-    dispatch(increaseQuantity(id));
-  };
-
-  const handleDecrease = (id) => {
-    dispatch(decreaseQuantity(id));
-  };
-
   return (
     <div>
       <h2>Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ul>
-          {cartItems.map(item => (
-            <li key={item.id}>
-              {item.name} - Quantity: {item.quantity}
-              <button onClick={() => handleIncrease(item.id)}>Increase</button>
-              <button onClick={() => handleDecrease(item.id)}>Decrease</button>
-              <button onClick={() => handleRemove(item.id)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-      )}
+      {cartItems.map(item => (
+        <div key={item.id} className="cart-item">
+          <span>{item.name}</span>
+          <button onClick={() => dispatch(increaseQuantity(item.id))}>+</button>
+          <button onClick={() => dispatch(decreaseQuantity(item.id))}>-</button>
+          <button onClick={() => dispatch(removeFromCart(item.id))}>Remove</button>
+        </div>
+      ))}
     </div>
   );
 };
