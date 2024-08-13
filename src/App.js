2010de6e171aacddf6d6cddc57e140e40ce13bed
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 19:23:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Provider, useSelector } from 'react-redux';
-import store from './store';
+import { useSelector } from 'react-redux';
 import Product from './components/Product';
 import Cart from './components/Cart';
 import Wishlist from './components/Wishlist';
@@ -11,16 +10,14 @@ const App = () => {
   const wishlist = useSelector(state => state.wishlist);
 
   return (
-    <Provider store={store}>
-      <div>
-        <h1>Shopping Cart Application</h1>
-        <Product product={{ id: 1, name: 'Product 1' }} />
-        <Product product={{ id: 2, name: 'Product 2' }} />
-        <Cart cartItems={cart} />
-        <Wishlist />
-        <Coupon />
-      </div>
-    </Provider>
+    <div>
+      <h1>Shopping Cart Application</h1>
+      <Product product={{ id: 1, name: 'Product 1' }} />
+      <Product product={{ id: 2, name: 'Product 2' }} />
+      <Cart cartItems={cart} />
+      <Wishlist />
+      <Coupon />
+    </div>
   );
 };
 
