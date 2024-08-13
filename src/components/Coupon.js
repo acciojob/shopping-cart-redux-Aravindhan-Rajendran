e5d17b42df78407d4a0c8976e3cab9f8f98eab1c
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 19:29:14 +0530

    Update Coupon.js

--- src/components/Coupon.js
+++ src/components/Coupon.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { applyDiscount } from '../actions';
+import React from 'react';
 
 const Coupon = () => {
-  const [discountCode, setDiscountCode] = useState('');
-  const dispatch = useDispatch();
-
-  const handleApplyCoupon = () => {
-    dispatch(applyDiscount(discountCode));
-    setDiscountCode('');
-  };
-
   return (
     <div>
-      <h2>Apply Coupon</h2>
-      <input
-        type="text"
-        value={discountCode}
-        onChange={(e) => setDiscountCode(e.target.value)}
-        placeholder="Enter coupon code"
-      />
-      <button onClick={handleApplyCoupon}>Apply</button>
+      <h2>Coupon</h2>
+      <input type="text" placeholder="Enter coupon code" />
+      <button>Apply</button>
     </div>
   );
 };
